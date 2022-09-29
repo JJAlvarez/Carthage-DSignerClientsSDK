65ce7e77112b80fda0c4f85bd1af3f9fa7c33fbb
Javier Alvarez <javierj.alvarez@wundermanthompson.com>
2022-09-29 04:16:50 -0600

    Initial Commit

---
+++ Carthage-DSignerClientsSDK/Carthage_DSignerClientsSDK.h
@@ -0,0 +1,18 @@
+//
+//  Carthage_DSignerClientsSDK.h
+//  Carthage-DSignerClientsSDK
+//
+//  Created by Javier Alvarez on 29/09/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Carthage_DSignerClientsSDK.
+FOUNDATION_EXPORT double Carthage_DSignerClientsSDKVersionNumber;
+
+//! Project version string for Carthage_DSignerClientsSDK.
+FOUNDATION_EXPORT const unsigned char Carthage_DSignerClientsSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Carthage_DSignerClientsSDK/PublicHeader.h>
+
+
